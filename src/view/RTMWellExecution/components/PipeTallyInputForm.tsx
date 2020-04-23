0b1633bb2e96badf78a024f00a2114a7d008a9f6
kidtango <fq600@yahoo.com>
2020-04-22 23:46:01 -0500

    push it

--- src/view/RTMWellExecution/components/PipeTallyInputForm.tsx
+++ src/view/RTMWellExecution/components/PipeTallyInputForm.tsx
@@ -1,28 +1,87 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import { Button } from '@material-ui/core';
+import validate from 'validate.js';
+
+const formSchema = {
+  depth: {
+    presence: { allowEmpty: false, message: 'Depth in ft. is required' },
+    type: 'string',
+  },
+  pressure: {
+    presence: { allowEmpty: false, message: 'Pressure in paia is required' },
+    type: 'string',
+  },
+  event: {
+    presence: {
+      allowEmpty: false,
+      message: 'Please fill in event description',
+    },
+    type: 'string',
+  },
+};
 
 interface State {
-  amount: string;
-  password: string;
-  weight: string;
-  weightRange: string;
-  showPassword: boolean;
+  values: {
+    depth: number | null;
+    pressure: number | null;
+    event: string;
+  };
+  isValid: boolean;
+  touched: {
+    depth: boolean;
+    pressure: boolean;
+    event: boolean;
+  };
+  errors: {
+    depth: string[];
+    pressure: string[];
+    event: string[];
+  };
 }
 
 const PipeTallyInputForm: React.FC = () => {
-  const [values, setValues] = React.useState<State>({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
+  const [formState, setFormState] = useState<State>({
+    values: { depth: null, pressure: null, event: '' },
+    isValid: false,
+    touched: { depth: false, pressure: false, event: false },
+    errors: { depth: [''], pressure: [''], event: [''] },
   });
+
+  useEffect(() => {
+    const errors = validate(formState.values, formSchema);
+
+    setFormState((formState) => ({
+      ...formState,
+      isValid: errors ? false : true,
+      errors: errors || {},
+    }));
+  }, [formState.values]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+
+    setFormState((formState) => ({
+      ...formState,
+      values: {
+        ...formState.values,
+        [event.target.name]: event.target.value,
+      },
+      touched: {
+        ...formState.touched,
+        [event.target.name]: true,
+      },
+    }));
+  };
+
+  const hasError = (
+    field: keyof typeof formState.touched | keyof typeof formState.errors
+  ) => (formState.touched[field] && formState.errors[field] ? true : false);
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
@@ -30,15 +89,22 @@ const PipeTallyInputForm: React.FC = () => {
         className={clsx(classes.margin, classes.textField)}
         variant='outlined'
       >
-        <InputLabel htmlFor='outlined-adornment-password' margin='dense'>
+        <InputLabel
+          htmlFor='outlined-adornment-depth'
+          margin='dense'
+          error={hasError('depth')}
+          required={true}
+        >
           enter depth in ft
         </InputLabel>
         <OutlinedInput
-          id='outlined-adornment-password'
-          type={'text'}
-          // value={values.password}
-          // onChange={handleChange('password')}
-
+          error={hasError('depth')}
+          id='outlined-adornment-depth'
+          name='depth'
+          type={'number'}
+          required={true}
+          value={formState.values && formState.values.depth}
+          onChange={handleChange}
           labelWidth={100}
           classes={{ root: classes.outlinedInput }}
         />
@@ -47,15 +113,21 @@ const PipeTallyInputForm: React.FC = () => {
         className={clsx(classes.margin, classes.textField)}
         variant='outlined'
       >
-        <InputLabel htmlFor='outlined-adornment-password' margin='dense'>
+        <InputLabel
+          htmlFor='outlined-adornment-pressure'
+          margin='dense'
+          required={true}
+          error={hasError('pressure')}
+        >
           enter pressure in paia
         </InputLabel>
         <OutlinedInput
-          id='outlined-adornment-password'
-          type={'text'}
-          // value={values.password}
-          // onChange={handleChange('password')}
-
+          error={hasError('pressure')}
+          id='outlined-adornment-pressure'
+          name='pressure'
+          type={'number'}
+          value={formState.values && formState.values.pressure}
+          onChange={handleChange}
           labelWidth={135}
           classes={{ root: classes.outlinedInput }}
         />
@@ -64,18 +136,23 @@ const PipeTallyInputForm: React.FC = () => {
         className={clsx(classes.margin, classes.descriptionTextField)}
         variant='outlined'
       >
-        <InputLabel htmlFor='outlined-adornment-password' margin='dense'>
+        <InputLabel
+          htmlFor='outlined-adornment-event'
+          margin='dense'
+          required={true}
+          error={hasError('event')}
+        >
           describe event
         </InputLabel>
         <OutlinedInput
-          id='outlined-adornment-password'
+          error={hasError('event')}
+          id='outlined-adornment-event'
           type={'text'}
-          // value={values.password}
-          // onChange={handleChange('password')}
-
+          name='event'
+          value={(formState?.values && formState?.values.event) || ''}
+          onChange={handleChange}
           labelWidth={100}
           classes={{ root: classes.outlinedInput }}
-          error={false}
         />
       </FormControl>
       <div className={classes.buttonGroup}>
@@ -84,6 +161,8 @@ const PipeTallyInputForm: React.FC = () => {
           color='primary'
           className={classes.submit}
           classes={{ root: classes.buttonRoot }}
+          disabled={!formState.isValid}
+          type='submit'
         >
           submit
         </Button>
@@ -114,7 +193,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
 
     textField: {
-      width: '20%',
+      width: '18%',
     },
     descriptionTextField: {
       flex: 1,
@@ -124,7 +203,7 @@ const useStyles = makeStyles((theme: Theme) =>
     submit: { marginRight: theme.spacing(1) },
     clear: {},
     buttonRoot: { width: 70, height: 40 },
-    outlinedInput: { height: 40, padding: 0, alignItems: 'end' },
+    outlinedInput: { height: 40, padding: 0, alignItems: 'center' },
   })
 );
 
